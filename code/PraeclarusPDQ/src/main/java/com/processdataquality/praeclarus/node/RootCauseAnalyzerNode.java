8e7181aaddb914f2899b00679e4a31245d6902a6
NirashaSewwandi <e18329@eng.pdn.ac.lk>
2024-06-17 23:50:24 +0530

    Add complete implementation on getting answers from the users

--- code/PraeclarusPDQ/src/main/java/com/processdataquality/praeclarus/node/RootCauseAnalyzerNode.java
+++ code/PraeclarusPDQ/src/main/java/com/processdataquality/praeclarus/node/RootCauseAnalyzerNode.java
@@ -23,12 +23,14 @@ import com.processdataquality.praeclarus.rootCause.AbstractRootCause;
 import com.processdataquality.praeclarus.ui.component.announce.Announcement;
 import com.processdataquality.praeclarus.ui.component.dialog.Questions;
 
+import tech.tablesaw.api.BooleanColumn;
+import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
-
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.ArrayList;
 
 /**
@@ -40,6 +42,7 @@ public class RootCauseAnalyzerNode extends Node {
 
         private List<List<String>> questionsList = new ArrayList<>();
         private Table detected;
+        private int questionsCount = 0;
 
         public RootCauseAnalyzerNode(AbstractPlugin plugin) {
                 super(plugin);
@@ -52,111 +55,106 @@ public class RootCauseAnalyzerNode extends Node {
          */
         @Override
         public void run() throws Exception {
-                //Get root cause plugin
                 AbstractRootCause questions = (AbstractRootCause) getPlugin();
 
-                try {
-                        //Common Questions for each imperfection pattern
-                        // Distorted Label
-                        if (questions.getName().compareTo("Distorted Label") == 0) {
-                                List<String> QList_DataEntry = new ArrayList<>();
+                Announcement.success(questions.getName());
 
-                                QList_DataEntry.add("Data Entry Interface");
-                                QList_DataEntry.add("Do your IT systems allow users to modify or overwrite automatically generated activity labels?");
-                                QList_DataEntry.add("How is this functionality justified?");
-                                QList_DataEntry.add(
-                                                "Do your data entry tools have any features to validate or warn users about potential inconsistencies when modifying activity labels?");
-                                questionsList.add(QList_DataEntry);
+                if (questions.getName().compareTo("Distorted Label") == 0) {
+                        List<String> QList_DataEntry = new ArrayList<>();
+                        QList_DataEntry.add("Data Entry Interface");
+                        QList_DataEntry.add(
+                                        "Do your IT systems allow users to modify or overwrite automatically generated activity labels?");
+                        QList_DataEntry.add("How is this functionality justified?");
+                        QList_DataEntry.add(
+                                        "Do your data entry tools have any features to validate or warn users about potential inconsistencies when modifying activity labels?");
+                        questionsList.add(QList_DataEntry);
 
-                                List<String> QList_Config = new ArrayList<>();
-                                QList_Config.add("System Configuration");
-                                QList_Config.add("Do you have different IT systems in your process?");
-                                QList_Config.add(
-                                                "Do these systems use a standardized vocabulary or controlled list for activity labels?");
-                                QList_Config.add(
-                                                "How are inconsistencies between different systems managed during data integration?");
-                                questionsList.add(QList_Config);
-                                                List<String> QList_Manual = new ArrayList<>();
-                                QList_Manual.add("Manual Data Entry");
-                                QList_Manual.add("Is any activity label information entered manually during your data collection process?");
-                                QList_Manual.add("Do different process participants use slightly different terminology or abbreviations for the same activity?");
-                                QList_Manual.add("Do your data entry tools have any features to suggest or enforce a standardized vocabulary for activity labels?");
-                                questionsList.add(QList_Manual);
+                        List<String> QList_Config = new ArrayList<>();
+                        QList_Config.add("System Configuration");
+                        QList_Config.add("Do you have different IT systems in your process?");
+                        QList_Config.add(
+                                        "Do these systems use a standardized vocabulary or controlled list for activity labels?");
+                        QList_Config.add(
+                                        "How are inconsistencies between different systems managed during data integration?");
+                        questionsList.add(QList_Config);
 
-                        }
-                        // Polluted Label
-                        else if (questions.getName().compareTo("Polluted Label") == 0) {
-                                List<String> QList_DataField = new ArrayList<>();
-                                QList_DataField.add( "Data Field Design");
-                                QList_DataField.add(
-                                                "Do your IT systems allow for recording multiple values within a single event attribute field?");
-                                QList_DataField.add("How is this functionality justified?");
-                                QList_DataField.add(
-                                                "Are there any plans to redesign the data capture process to separate these values into distinct attributes?");
-                                questionsList.add(QList_DataField);
-                                                List<String> QList_Manual = new ArrayList<>();
-                                QList_Manual.add( "Manual Data Entry ");
-                                QList_Manual.add(
-                                                "Is any information in the polluted attribute fields entered manually during data collection? "      );
-                                QList_Manual.add(
-                                                "Do different process participants use varying formats or conventions when entering data into these fields?");
-                                QList_Manual.add(
-                                                "Do your data entry tools have any features to guide users towards consistent formatting in these fields?");
-                                questionsList.add(QList_Manual);
+                        List<String> QList_Manual = new ArrayList<>();
 
-                        }
-                        // Synonymous Label
-                        else if (questions.getName().compareTo("Synonymous Label") == 0) {
-                                List<String> QList_Multiple = new ArrayList<>();
-                                QList_Multiple.add("Multiple Systems");
-                                QList_Multiple.add(
-                                                "Do you use different IT systems to capture data for the process you're analyzing?");
-                                QList_Multiple.add(
-                                                "Do these systems use the same terminology for process steps and attributes?"
-                                                );
-                                List<String> QList_Config = new ArrayList<>();
-                                QList_Config.add( "System Configuration");
-                                QList_Config.add(
-                                                "Do your IT systems allow users to customize how they record data (e.g., labels, attributes)?"
-                                                );
-                                QList_Config.add(
-                                                "Are there any departmental or user-level configurations that might lead to variations in how the same data is recorded?"
-                                                );
-                                List<String> QList_Data = new ArrayList<>();
-                                QList_Data.add( "Data Transformation");
-                                QList_Data.add(
-                                                "Do you have any data transformation processes (ETL) before using the data for process mining?"
-                                               );
-                                QList_Data.add(
-                                                "Do these data transformation tools have functionalities to modify or standardize labels or attribute values?"
-                                               );
-                                QList_Data.add(
-                                                "Are these tools configured to handle synonymous labels?");
-                                QList_Data.add("How do they handle them (e.g., mapping, merging)?");
-                                QList_Data.add(
-                                                "Are data curators or analysts involved in overseeing or configuring these transformations?"
-                                               );
-                                QList_Data.add(
-                                                "What training or guidelines are in place to ensure consistent handling of synonymous labels?"
-                                                );
+                        QList_Manual.add("Manual Data Entry");
+                        QList_Manual.add(
+                                        "Is any activity label information entered manually during your data collection process?");
+                        QList_Manual.add(
+                                        "Do different process participants use slightly different terminology or abbreviations for the same activity?");
+                        QList_Manual.add(
+                                        "Do your data entry tools have any features to suggest or enforce a standardized vocabulary for activity labels?");
+                        questionsList.add(QList_Manual);
 
-                        }
+                        questionsCount = 9;
 
-                        Announcement.show("Please answer to the below questions to proceed !!!");
-                        
-                        // setState(NodeState.COMPLETED);
-                } catch (Exception e) {
-
-                        // see if file was loaded previously, then stored
-                        String tableID = getTableID();
-                        if (!(tableID == null || tableID.isEmpty())) {
-                                loadOutput(tableID); // load from repo
-
-                                // setState not used here because loadOutput() above sets it to completed
-                                announceStateChange();
-                        } else
-                                throw new ReaderException(e.getMessage(), e.getCause());
                 }
+
+                else if (questions.getName().compareTo("Polluted Label") == 0) {
+
+                        List<String> QList_DataField = new ArrayList<>();
+                        QList_DataField.add("Data Field Design");
+                        QList_DataField.add(
+                                        "Do your IT systems allow for recording multiple values within a single event attribute field?");
+                        QList_DataField.add("How is this functionality justified?");
+                        QList_DataField.add(
+                                        "Are there any plans to redesign the data capture process to separate these values into distinct attributes?");
+                        questionsList.add(QList_DataField);
+
+                        List<String> QList_Manual = new ArrayList<>();
+                        QList_Manual.add("Manual Data Entry ");
+                        QList_Manual.add(
+                                        "Is any information in the polluted attribute fields entered manually during data collection? ");
+                        QList_Manual.add(
+                                        "Do different process participants use varying formats or conventions when entering data into these fields?");
+                        QList_Manual.add(
+                                        "Do your data entry tools have any features to guide users towards consistent formatting in these fields?");
+                        questionsList.add(QList_Manual);
+                        questionsCount = 6;
+
+                }
+
+                else if (questions.getName().compareTo("Synonymous Label") == 0) {
+
+                        List<String> QList_Multiple = new ArrayList<>();
+                        QList_Multiple.add("Multiple Systems");
+                        QList_Multiple.add(
+                                        "Do you use different IT systems to capture data for the process you're analyzing?");
+                        QList_Multiple.add(
+                                        "Do these systems use the same terminology for process steps and attributes?");
+                        questionsList.add(QList_Multiple);
+
+                        List<String> QList_Config = new ArrayList<>();
+                        QList_Config.add("System Configuration");
+                        QList_Config.add(
+                                        "Do your IT systems allow users to customize how they record data (e.g., labels, attributes)?");
+                        QList_Config.add(
+                                        "Are there any departmental or user-level configurations that might lead to variations in how the same data is recorded?");
+                        questionsList.add(QList_Config);
+
+                        List<String> QList_Data = new ArrayList<>();
+                        QList_Data.add("Data Transformation");
+                        QList_Data.add(
+                                        "Do you have any data transformation processes (ETL) before using the data for process mining?");
+                        QList_Data.add(
+                                        "Do these data transformation tools have functionalities to modify or standardize labels or attribute values?");
+                        QList_Data.add(
+                                        "Are these tools configured to handle synonymous labels?");
+                        QList_Data.add("How do they handle them (e.g., mapping, merging)?");
+                        QList_Data.add(
+                                        "Are data curators or analysts involved in overseeing or configuring these transformations?");
+                        QList_Data.add(
+                                        "What training or guidelines are in place to ensure consistent handling of synonymous labels?");
+                        questionsList.add(QList_Data);
+                        questionsCount = 10;
+
+                }
+
+                Announcement.success("Please answer to the below questions to proceed !!!");
+
                 if (getState() == NodeState.UNSTARTED) {
 
                         // load plugin with all incoming plugins' aux datasets
@@ -164,13 +162,51 @@ public class RootCauseAnalyzerNode extends Node {
 
                         setState(NodeState.EXECUTING);
 
-                        //Build the Questions dialog and pass the listener and questions list
-                        Questions dialog = new Questions(e -> {
-                                if (e.successful) {}
-                        }, questionsList);
+                        Announcement.show("Please answer to the below questions to proceed !!!");
 
-                        //Open the dialog 
+                        // Build the Questions dialog and pass the listener and questions list
+                        Questions dialog = new Questions(e -> {
+                                if (e.successful) {
+
+                                }
+                        }, questionsList, questionsCount);
+
+                        // Open the dialog
                         dialog.open();
+
+                        detected = Table.create("Questions").addColumns(StringColumn.create("Category"),
+                                        StringColumn.create("Questions"), BooleanColumn.create("Answers"));
+
+                        Thread waiterThread = new Thread(() -> {
+                                try {
+                                        System.out.println("Waiting for variable to change...");
+                                        dialog.getResults().thenRun(() -> {
+                                                System.out.println("Variable changed! New value: "
+                                                                + dialog.getWatchedVariable());
+
+                                                for (List<String> list : questionsList) {
+
+                                                        for (String var : list.subList(1, list.size())) {
+                                                                detected.stringColumn(0).append(list.get(0));
+                                                                detected.stringColumn(1).append(var);
+
+                                                        }
+
+                                                }
+                                                for (Boolean x : dialog.getWatchedVariable()) {
+                                                        detected.booleanColumn(2).append(x);
+                                                }
+                                        }).get(); // Block until the variable changes
+                                        setOutput(detected);
+                                } catch (Exception e) {
+                                        e.printStackTrace();
+                                }
+                        });
+                        waiterThread.start();
+
+                        setOutput(detected);
+
+                        setState(NodeState.COMPLETED);
 
                 }
 
--- code/PraeclarusPDQ/src/main/java/com/processdataquality/praeclarus/ui/component/dialog/Questions.java
+++ code/PraeclarusPDQ/src/main/java/com/processdataquality/praeclarus/ui/component/dialog/Questions.java
@@ -17,117 +17,99 @@
 package com.processdataquality.praeclarus.ui.component.dialog;
 
 import com.processdataquality.praeclarus.ui.component.announce.Announcement;
-import com.vaadin.flow.component.HtmlComponent;
-import com.vaadin.flow.component.Tag;
+
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
-
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.upload.Upload;
-import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.component.html.H5;
 import com.vaadin.flow.component.html.H4;
-import com.vaadin.flow.component.html.Span;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.beans.PropertyChangeSupport;
+import java.beans.PropertyChangeListener;
+import java.util.Arrays;
 
 /**
  * @author Michael Adams
  * @date 27/5/21
  */
 public class Questions extends AbstractDialog {
+    private Boolean[] listOfResponses;
+    private final CompletableFuture<Void> signal = new CompletableFuture<>();
 
-    public Questions(UploadDialogListener listener, List<List<String>> questionsList) {
+    public Questions(QuestionDialogListener listener, List<List<String>> questionsList, int questionsCount) {
+
         super("Questionnaire");
-        MemoryBuffer buffer = new MemoryBuffer();
-        Div div = new Div();
-        // RadioButtonGroup button = new RadioButtonGroup<>();
-        // button.setItems("True", "False");
-        // H3 title;
-        // H5 question;
-        // RadioButtonGroup button;
-        
-        // Map <H5, RadioButtonGroup<String>> x = new HashMap<>();
-        for (List<String> entry : questionsList){
-            H4 title = new H4(entry.get(0));
-            addComponent(title);
 
-            for(String val : entry.subList(1, entry.size())){
-                H5 question = new H5(val);
-                addComponent(question);
-                RadioButtonGroup<String> button = new RadioButtonGroup<>();
-                button.setItems("True", "False");
-                addComponent(button);
-            }
-            
-               
+        // Get the user response
+        listOfResponses = new Boolean[questionsCount];
 
-        }
-
-        
-        // for (Map.Entry<H5, RadioButtonGroup<String>> entry : x.entrySet()) {
-        //     if(entry.getValue()==null){
-        //         addComponent(entry.getKey());
-
-        //     }
-        //     else{
-        //         addComponent(entry.getKey());
-        //         addComponent(entry.getValue());
-
-        //     }
-            
-
-        // }
-
-        // H5 Q2 = new H5(
-        //         "Do your data entry tools have any features to validate or warn users about potential inconsistencies when modifying activity labels?");
-        // Span Q3 = new Span(
-        //         "Do your data entry tools have any features to validate or warn users about potential inconsistencies when modifying activity labels?");
-
-        // TextField Q1 = new TextField(
-        //         "Do your data entry tools have any features to validate or warn users about potential inconsistencies when modifying activity labels?");
-
-        // addComponent(Q1);
-        // addComponent(Q2);
-        // addComponent(button);
-        // private TextField firstname;
-
+        // OK Button
         Button ok = new Button("OK", event -> {
-            listener.dialogClosed(new UploadDialogCloseEvent(true,
-                    buffer.getInputStream(), buffer.getFileName()));
+            listener.dialogClosed(new QuestionDialogCloseEvent(true));
+
             close();
         });
         ok.setEnabled(false);
         ok.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-        Upload upload = new Upload(buffer);
-        Div outputMsg = new Div();
+        int mainIndex = 0;
 
-        // upload.setAcceptedFileTypes(mimeDescriptors);
-        upload.addSucceededListener(event -> ok.setEnabled(true));
+        // Add question components to the dialog
+        for (List<String> entry : questionsList) {
+            // Title as h4 tag
+            H4 title = new H4(entry.get(0));
+            addComponent(title);
 
-        // upload.getElement().addEventListener("file-remove",
-        // event -> outputMsg.removeAll());
+            // Loop through the all questions for that title
+            for (String val : entry.subList(1, entry.size())) {
 
-        // upload.addFileRejectedListener(event -> {
-        // outputMsg.removeAll();
-        // HtmlComponent p = new HtmlComponent(Tag.P);
-        // p.getElement().setText(event.getErrorMessage());
-        // outputMsg.add(p);
-        // ok.setEnabled(false);
-        // });
+                // Question as h5 tag
+                H5 question = new H5(val);
+                addComponent(question);
+
+                // Selection as True or false
+                RadioButtonGroup<String> button = new RadioButtonGroup<>();
+                button.setItems("True", "False");
+                final Integer index = mainIndex;
+                // Add button listener to get response
+                button.addValueChangeListener(event -> {
+                    // Get the selected value and record them
+                    if (button.getValue() == "True") {
+                        listOfResponses[index] = true;
+                    } else if (button.getValue() == "False") {
+                        listOfResponses[index] = false;
+                    }
+                    // Only if all the answers are given , then show up the ok button
+                    if (!Arrays.asList(listOfResponses).contains(null)) {
+                        ok.setEnabled(true);
+                        signal.complete(null);
+                    }
+                    
+                });
+
+                mainIndex += 1;
+
+                addComponent(button);
+            }
+
+        }
 
         Button cancel = new Button("Cancel", event -> {
-            listener.dialogClosed(new UploadDialogCloseEvent(false, null, null));
+            listener.dialogClosed(new QuestionDialogCloseEvent(false));
             close();
         });
 
-        // addComponent(upload, outputMsg);
         getButtonBar().add(cancel, ok);
+    }
+
+    public CompletableFuture<Void> getResults() {
+        return signal;
+    }
+
+    public Boolean[] getWatchedVariable() {
+        return listOfResponses;
     }
 
 }
